712ea214a9fa443bbd407bc7baad602f4a074522
Luis Fernando Galviz <lgalviz@uninorte.edu.co>
2024-04-11 12:02:14 -0500

    feat: 🎸 add new background for cards

--- final-exams/src/client/components/Card.jsx
+++ final-exams/src/client/components/Card.jsx
@@ -9,7 +9,7 @@ import CardContentComponent from "./CardContentComponent";
 const useStyles = makeStyles({
   root: {
     minWidth: 275,
-    backgroundColor: "#A7C9E6",
+    backgroundColor: "#E6E6FA",
     borderRadius: 10,
     boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
     marginTop: 10,
