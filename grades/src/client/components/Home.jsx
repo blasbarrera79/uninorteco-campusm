1291238993a91758ae644e8249dbc3d36439e296
Luis Fernando Galviz <lgalviz@uninorte.edu.co>
2024-04-29 12:58:09 -0500

    feat: 🎸 home component functionalities

--- grades/src/client/components/Home.jsx
+++ grades/src/client/components/Home.jsx
@@ -1,11 +1,13 @@
-import React, { useState, useEffect } from "react"
-import { makeStyles } from "@material-ui/core/styles"
-import Container from "@material-ui/core/Container"
-import GradeCard from "./GradeCard"
-import SelectComponent from "./SelectComponent"
-import { getRegistration } from "../services/registrationService"
-import { getGrades } from "../services/gradeService"
-import { Typography, CircularProgress } from "@material-ui/core"
+import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { CircularProgress } from "@material-ui/core";
+import Container from "@material-ui/core/Container";
+import GradeCard from "./GradeCard";
+import SelectComponent from "./SelectComponent";
+import { getRegistration } from "../services/registrationService";
+import { getGrades } from "../services/gradeService";
+import { getTerm } from "../services/termService";
+import { getUser } from "../services/userService";
 
 const useStyles = makeStyles({
   container: {
@@ -17,79 +19,92 @@ const useStyles = makeStyles({
     alignItems: "center",
     height: "100vh",
   },
-})
+});
 
 const Screen = () => {
-  const [subjectsGrades, setSubjectsGrades] = useState([])
-  const [error, setError] = useState(null)
-  const [selectedValue, setSelectedValue] = useState("202310")
-  const [isLoading, setIsLoading] = useState(false) // Estado para controlar el loader
-  const classes = useStyles()
-
-  const options = [
-    { value: "202310", label: "202310" },
-    { value: "202330", label: "202330" },
-    { value: "202410", label: "202410" },
-    { value: "202411", label: "202411" },
-    { value: "202400", label: "202400" },
-  ]
+  const [subjectsGrades, setSubjectsGrades] = useState([]);
+  const [selectedTerm, setSelectedTerm] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [terms, setTerms] = useState([]);
+  const [user, setUser] = useState('');
+  const classes = useStyles();
 
   useEffect(() => {
     const fetchData = async () => {
-      setIsLoading(true)
+      setIsLoading(true);
       try {
-        const registration = await getRegistration(selectedValue)
+        const termsResponse = await getTerm();
+        setTerms(termsResponse);
+        setSelectedTerm(termsResponse[0].PERIODO);
+        const userResponse = await getUser();
+        console.log('userResponse',userResponse);
+        setUser(userResponse);
+        setIsLoading(false);
+      } catch (err) {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        console.log('selectedTerm',selectedTerm);
+        const registration = await getRegistration(selectedTerm);
+        console.log('registration',registration);
         const promises = registration.map(async (element) => {
-          const grades = await getGrades(element.SFRSTCR_CRN)
+          console.log(user,element.SFRSTCR_CRN, selectedTerm)
+          const grades = await getGrades(user,element.SFRSTCR_CRN, selectedTerm);
           return {
             materia: element.SSBSECT_CRSE_TITLE,
             items: grades.map((item) => ({
               name: item.SHRGCOM_NAME,
               value: item.NOTA,
             })),
-          }
-        })
+          };
+        });
 
-        const subjectsGrades = await Promise.all(promises)
-        setSubjectsGrades(subjectsGrades)
-        setIsLoading(false)
-      } catch (error) {
-        setIsLoading(false)
-        setError(error)
+        const subjectsGradesResponse = await Promise.all(promises);
+        setSubjectsGrades(subjectsGradesResponse);
+        setIsLoading(false);
+      } catch (err) {
+        setIsLoading(false);
       }
-    }
+    };
 
-    fetchData()
-  }, [selectedValue])
+    fetchData();
+  }, [selectedTerm, user]);
 
   const handleSelectChange = (event) => {
-    setSelectedValue(event.target.value)
-  }
+    setSelectedTerm(event.target.value);
+  };
 
   return (
     <Container className={classes.container}>
       <SelectComponent
         label="Selecciona una opción"
-        value={selectedValue}
+        value={selectedTerm}
         onChange={handleSelectChange}
-        options={options}
+        options={terms}
       />
       {isLoading ? (
         <div className={classes.loaderContainer}>
           <CircularProgress />
         </div>
       ) : (
-        subjectsGrades.map((subjectGrade, index) => (
+        subjectsGrades.map((subjectGrade) => (
           <GradeCard
-            key={index}
+            key={subjectGrade.materia}
             gradeName={subjectGrade.materia}
             items={subjectGrade.items}
           />
         ))
       )}
-      <Typography>{error}</Typography>
     </Container>
-  )
-}
+  );
+};
 
-export default Screen
+export default Screen;
