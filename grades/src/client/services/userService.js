9df3a1f1837cb84a2bc9784fd1196e9f9773ecab
Luis Fernando Galviz <lgalviz@uninorte.edu.co>
2024-04-29 22:44:21 -0500

    feat: 🎸 userService

---
+++ grades/src/client/services/userService.js
@@ -0,0 +1,16 @@
+import { request } from "@ombiel/aek-lib"
+
+export const getUser = () => {
+  return new Promise((resolve, reject) => {
+    request
+      .action("get-user")
+      .end((err, res) => {
+        if (err) {
+          reject(err)
+        } else {
+          console.log(res.body)
+          resolve(res.body.username)
+        }
+      })
+  })
+}
