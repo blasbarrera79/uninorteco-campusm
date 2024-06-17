9c3f1e30187a4fa6ff683caa1b69b4c0f85c3e77
yonyhome <yonyh@uninorte.edu.co>
2024-06-17 10:50:03 -0500

    correct folder

--- bibliographic-services/bibliographic-services/src/client/servicios-bibliograficos.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "react-dom";
-import {reactRender} from "@ombiel/aek-lib";
-
-import DatabaseComponent from "./servicios-bibliograficos/screen";
-
-reactRender(<DatabaseComponent />);
