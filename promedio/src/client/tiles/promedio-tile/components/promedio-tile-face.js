0bf03c4e6f11ef8e777a9a6f8290c1d0b3371ee3
yonyhome <yonyh@uninorte.edu.co>
2024-06-19 14:24:44 -0500

    only image

--- promedio/src/client/tiles/promedio-tile/components/promedio-tile-face.js
+++ promedio/src/client/tiles/promedio-tile/components/promedio-tile-face.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Box, Typography, Avatar, Grid } from '@mui/material';
-
+import { Box, Grid } from '@mui/material';
+import { TileFace } from '@ombiel/cm-tile-sdk';
 const services = [
   { id: 1, name: 'Mis Notas', image: 'https://portal-na.campusm.exlibrisgroup.com/assets/UniversidaddelNorte/UniversidaddelNorte/icons-rounded/mis-notas-rounded.png', url: 'campusm://loadaek?toolbar=AEK1000030212' },
   { id: 2, name: 'Evaluación Docente', image: 'https://portal-na.campusm.exlibrisgroup.com/assets/UniversidaddelNorte/UniversidaddelNorte/icons-rounded/evaluacion-docente-rounded.png', url: 'campusm://openURL?url=https://pomelo.uninorte.edu.co/evaluaciondoc/index.zul' },
@@ -14,50 +14,45 @@ const services = [
   { id: 10, name: '¿Necesitas Ayuda?', image: 'https://portal-na.campusm.exlibrisgroup.com/assets/UniversidaddelNorte/UniversidaddelNorte/icons-rounded/ayuda-rounded.png', url: 'campusm://loadaek?toolbar=AEK1000030219' },
 ];
 
-
-
 export default function ServicesList() {
   return (
-    <Box bgcolor="#f5f5f5" p={1}>
-      <Grid container spacing={1} justifyContent="center">
-        {services.map((service) => (
-          <Grid item key={service.id} xs={6} md={6}>
-            <Box
-              component="a"
-              href={service.url}
-              target="_blank"
-              rel="noopener noreferrer"
-              display="flex"
-              alignItems="center"
-              p={1}
-              textDecoration="none"
-              color="inherit"
-              sx={{
-                transition: 'background-color 0.3s ease',
-                '&:hover': {
-                  backgroundColor: '#e0e0e0',
-                },
-                maxWidth: 180,
-                margin: 'auto',
-                textAlign: 'left', // Center align text
-              }}
-            >
-              <Avatar
-                alt={service.name}
-                src={service.image}
+    <TileFace>
+      <Box>
+        <Grid container justifyContent="center">
+          {services.map((service) => (
+            <Grid item key={service.id} xs={6} md={6}>
+              <Box
+                component="a"
+                href={service.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                display="flex"
+                justifyContent="center"
+                textDecoration="none"
+                color="inherit"
                 sx={{
-                  width: 60,
-                  height: 60,
-                  marginRight: 1,
+                  transition: 'background-color 0.3s ease',
+                  '&:hover': {
+                    backgroundColor: '#e0e0e0',
+                  },
+                  margin: 'auto',
+                  textAlign: 'center', // Center align text
                 }}
-              />
-              <Typography variant="subtitle1" fontWeight="bold" sx={{ fontSize: '0.7rem' }}>
-                {service.name}
-              </Typography>
-            </Box>
-          </Grid>
-        ))}
-      </Grid>
-    </Box>
+              >
+                <Box
+                  component="img"
+                  alt={service.name}
+                  src={service.image}
+                  sx={{
+                    width: '100%',
+                    height: 'auto',
+                  }}
+                />
+              </Box>
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
+    </TileFace>
   );
 }
