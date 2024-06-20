baafe8f49aa23e35dc373523cbed0277d4c833c7
yonyhome <yonyh@uninorte.edu.co>
2024-06-20 14:59:01 -0500

    new services

--- promedio/src/client/tiles/promedio-tile/components/promedio-tile-face.js
+++ promedio/src/client/tiles/promedio-tile/components/promedio-tile-face.js
@@ -9,8 +9,8 @@ const services = [
   { id: 6, name: 'Financiamiento', image: 'https://portal-na.campusm.exlibrisgroup.com/assets/UniversidaddelNorte/UniversidaddelNorte/icons-rounded/financiamiento-rounded.png', url: 'campusm://openURL?url=https://www.uninorte.edu.co/web/apoyo-financiero' },
   { id: 7, name: 'Enlaces de Interes', image: 'https://portal-na.campusm.exlibrisgroup.com/assets/UniversidaddelNorte/UniversidaddelNorte/icons-rounded/enlaces-interes-rounded.png', url: 'campusm://pocketguide?pg_code=1000029068' },
   { id: 8, name: 'Centro Medico', image: 'https://portal-na.campusm.exlibrisgroup.com/assets/UniversidaddelNorte/UniversidaddelNorte/icons-rounded/centro-medico-rounded.png', url: 'campusm://pocketguide?pg_code=1000029066' },
-  { id: 9, name: 'Buzón de sugerencias', image: 'https://portal-na.campusm.exlibrisgroup.com/assets/UniversidaddelNorte/UniversidaddelNorte/icons-rounded/buzon-rounded.png', url: 'campusm://openURL?url=https://www.uninorte.edu.co/sugerencias' },
-  { id: 10, name: '¿Necesitas Ayuda?', image: 'https://portal-na.campusm.exlibrisgroup.com/assets/UniversidaddelNorte/UniversidaddelNorte/icons-rounded/ayuda-rounded.png', url: 'campusm://loadaek?toolbar=AEK1000030219' },
+  { id: 9, name: 'Noticias', image: 'https://portal-na.campusm.exlibrisgroup.com/assets/UniversidaddelNorte/UniversidaddelNorte/icons-rounded/noticias-rounded.png', url: 'campusm://openURL?url=https://www.uninorte.edu.co/web/grupo-prensa' },
+  { id: 10, name: 'Eventos', image: 'https://portal-na.campusm.exlibrisgroup.com/assets/UniversidaddelNorte/UniversidaddelNorte/icons-rounded/eventos-rounded.png', url: 'campusm://openURL?url=https://www.uninorte.edu.co/web/eventos' },
 ];
 
 export default function ServicesList() {
